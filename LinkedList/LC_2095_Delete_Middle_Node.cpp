a84f3031a55b63de40081c12dff21a8a5c6a061d
dev-dg148 <debojeetgoswami148@gmail.com>
2026-06-15 08:53:14 +0530

    Solved LC 2095: Delete Middle Node (Medium - Micro-Optimized Tortoise-Hare, TC: O(N), SC: O(1))

---
+++ LinkedList/LC_2095_Delete_Middle_Node.cpp
@@ -0,0 +1,46 @@
+/*
+ * Problem: Delete the Middle Node of a Linked List (LeetCode 2095)
+ * Concept: Linked List, Tortoise and Hare (Micro-Optimized)
+ * TC: O(N) - N/2 iterations mathematically.
+ * SC: O(1) - Only 2 pointers (slow and fast) used.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        
+        if (head == NULL || head->next == NULL) {
+            return NULL;
+        }
+        
+        ListNode* s = head;
+        // Fast starts 2 steps ahead to make slow stop exactly BEFORE the middle node
+        ListNode* f = head->next->next; 
+        
+        while (f != NULL && f->next != NULL) {
+            s = s->next;
+            f = f->next->next;
+        }
+        
+        
+        ListNode* de = s->next;
+        s->next = de->next; 
+        
+        delete (de); 
+        
+        return head;
+    }
+};
